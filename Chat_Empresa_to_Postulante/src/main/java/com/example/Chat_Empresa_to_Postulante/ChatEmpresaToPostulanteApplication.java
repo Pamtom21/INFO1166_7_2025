d37669a3715c05d0d18d0533499f110fd4770b1f
Pamtom21 <joluis.2.11.04@gmail.com>
2025-08-30 02:44:59 -0400

    Proyecto creado, falta crear las carpetas para la infrastructura de arquitectura limpia

---
+++ Chat_Empresa_to_Postulante/src/main/java/com/example/Chat_Empresa_to_Postulante/ChatEmpresaToPostulanteApplication.java
@@ -0,0 +1,13 @@
+package com.example.Chat_Empresa_to_Postulante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatEmpresaToPostulanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatEmpresaToPostulanteApplication.class, args);
+	}
+
+}
---
+++ Chat_Empresa_to_Postulante/src/test/java/com/example/Chat_Empresa_to_Postulante/ChatEmpresaToPostulanteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Chat_Empresa_to_Postulante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatEmpresaToPostulanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
