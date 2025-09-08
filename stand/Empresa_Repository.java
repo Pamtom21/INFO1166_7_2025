2c5967844b7f865487969ca577de3aefe6e1a7f9
Pamtom21 <joluis.2.11.04@gmail.com>
2025-09-08 00:07:15 -0300

    eliminar excedentes

--- stand/Empresa_Repository.java
+++
@@ -1,7 +0,0 @@
-package com.domain.repository;
-
-import com.domain.model.empresa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Empresa_Repository extends JpaRepository<empresa, Long> {
-}
--- stand/PostulanteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.domain.repository;
-
-import com.domain.model.postulante;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostulanteRepository extends JpaRepository<postulante, String> {
-}
