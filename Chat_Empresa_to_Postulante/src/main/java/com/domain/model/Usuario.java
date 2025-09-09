d6bae2b797011e1d35e5b78a7e8f868e51bf5adf
JazielRoman <jroman2023@alu.uct.cl>
2025-09-09 15:52:54 -0400

    nuevos avancez de jaziel

--- Chat_Empresa_to_Postulante/src/main/java/com/domain/model/Usuario.java
+++ Chat_Empresa_to_Postulante/src/main/java/com/domain/model/Usuario.java
@@ -1,6 +1,11 @@
 package com.domain.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Usuario {
@@ -8,17 +13,45 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String nombre; //nombre del usuario
+    private String nombre; // nombre del usuario
+
+    private String correo; // correo del usuario
 
     @Enumerated(EnumType.STRING)
     private TipoUsuario tipo;
 
     public Usuario() {}
-//tipo de usuario empresa o postulante
-    public String getNombre(){return this.nombre; }
-    public void setNombre(String nombre){this.nombre = nombre;}
 
-    public TipoUsuario getTipo(){return this.tipo; }
-    public void setTipo(TipoUsuario tipo){this.tipo = tipo;}
-    //  Modelo de datos que guarda la informacion de usuarios..
+    // Getters y setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public TipoUsuario getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoUsuario tipo) {
+        this.tipo = tipo;
+    }
 }
