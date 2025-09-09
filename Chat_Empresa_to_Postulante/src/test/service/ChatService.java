7ab3370b393440faaf48b50ee4edaa97a9b01b2c
MATIASVLL18 <mvalenzuela2022@alu.uct.cl>
2025-09-08 22:07:33 -0300

    xd
    xd

--- Chat_Empresa_to_Postulante/src/test/service/ChatService.java
+++ Chat_Empresa_to_Postulante/src/test/service/ChatService.java
@@ -5,7 +5,7 @@ import com.example.Chat_Empresa_to_Postulante.model.Mensaje;
 import com.example.Chat_Empresa_to_Postulante.model.Usuario;
 import com.example.Chat_Empresa_to_Postulante.repository.MensajeRepository;
 import org.springframework.stereotype.Service;
-
+//import
 import java.util.List;
 
 @Service
@@ -24,6 +24,7 @@ public class ChatService {
 //devuelve la lista de mensajes entre dos usuarios
     public List<Mensaje> obtenerMensajes(Long remitenteId, Long destinatarioId) {
         return mensajeRepository.findByRemitenteIdAndDestinatarioId(remitenteId, destinatarioId);
+        
     }
 }
 //logica del microservicio encapsula la logica del chat ,separando controlador de la base de datos 
